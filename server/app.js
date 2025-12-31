ecc9e596daeed587460397a4b941eb40e3bbd1d6
Therese-Claire <awonguagabi@gmail.com>
2025-12-31 16:44:57 +0100

    Reorganized project folder structure

---
+++ server/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
